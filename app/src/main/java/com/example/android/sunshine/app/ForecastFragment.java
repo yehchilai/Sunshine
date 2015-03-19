9a3b20392258acabd4093e771bb36a13f9f0f067
Yeh-chi Lai <yehchilai@csupomona.edu>
2015-03-19 16:04:30 -0700

    1. Add new activity 2. Past weather data to new activity

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -99,7 +99,10 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String dayData = forecastAdaptor.getItem(position);
-                Toast.makeText(getActivity(), dayData, Toast.LENGTH_LONG).show();
+                //Toast.makeText(getActivity(), dayData, Toast.LENGTH_LONG).show();
+                Intent detailIntent = new Intent(getActivity(),DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, dayData);
+                startActivity(detailIntent);
             }
         });
 
